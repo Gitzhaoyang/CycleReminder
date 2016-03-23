daf5e15948be530f537ab6329e1e5162418cfe7f
mungo <mungo@hezhaoyangs-MBP.local>
2016-03-23 12:43:49 +0800

    commit to github

---
+++ CycleReminder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CycleReminder
+//
+//  Created by mungo on 23/03/16.
+//  Copyright © 2016 mungo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CycleReminder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CycleReminder
+//
+//  Created by mungo on 23/03/16.
+//  Copyright © 2016 mungo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
